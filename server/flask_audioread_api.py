f6523ccc8d4e4091b506715dd0f03a07c27033f1
nazeer2013 <nazeer.list@gmail.com>
2023-02-08 22:30:09 -0500

    logs to ui

--- server/flask_audioread_api.py
+++ server/flask_audioread_api.py
@@ -1,5 +1,7 @@
 # Flask API code to receive and process audio data
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
+import io
+import wave
 
 app = Flask(__name__)
 
@@ -17,7 +19,7 @@ def submit():
 
 # -------------------------------
 @app.route("/default")
-def index():
+def index2():
     return render_template("default.html")
 
 @app.route("/audio", methods=["POST"])
@@ -25,6 +27,53 @@ def receive_audio():
   audio_file = request.files["audio_data"]
   # process the audio file here
   return "Audio received successfully"
+
+@app.route('/api/data', methods=['GET', 'POST'])
+def handle_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        # process the data and save it
+        return jsonify({'message': 'Data received'})
+    else:
+        # retrieve the data and return it
+        return jsonify({'data': 'example data'})
+
+
+@app.route("/speech-to-text")
+def index3():
+    return render_template("speechtotext.html")
+
+
+@app.route("/api/speech-to-text", methods=["POST"])
+def speech_to_text():
+    print("Received request...1")
+    # Read audio data from request
+    audio = request.files["audio"]
+    audio_stream = io.BytesIO(audio.read())
+    print("Received request...1")
+    with wave.open(audio_stream, "rb") as wave_file:
+        audio_data = wave_file.readframes(wave_file.getnframes())
+
+    # Use SpeechRecognition library to recognize speech from audio data
+    recognized_text = recognize_speech(audio_data)
+
+    return recognized_text
+
+def recognize_speech(audio_data):
+    # Replace with your own speech recognition code
+    return "Recognized Text"
+
+@app.route("/logs", methods=["GET"])
+def logs():
+    logs = [
+        { "timestamp": "2022-12-01 12:00:00", "level": "INFO", "message": "Server started" },
+        { "timestamp": "2022-12-01 12:01:00", "level": "WARNING", "message": "Low disk space" },
+        { "timestamp": "2022-12-01 12:02:00", "level": "ERROR", "message": "Login failed" },
+        { "timestamp": "2022-12-01 12:03:00", "level": "INFO", "message": "Server stopped" },
+    ]
+
+    return jsonify(logs)
+    
 
 if __name__ == "__main__":
   app.run()
